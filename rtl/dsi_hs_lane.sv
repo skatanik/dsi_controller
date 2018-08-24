07a6619b8c637e46d2a9c65ee16cce536b75ef46
skatanik <muxaal@mail.ru>
2018-08-25 00:02:12 +0300

    [RTL] update

---
+++ rtl/dsi_hs_lane.sv
@@ -0,0 +1,138 @@
+module dsi_hs_lane(
+    input wire          clk_sys     , // serial data clock
+    input wire          clk_serdes  , // logic clock = clk_hs/8
+    input wire          clk_latch   , // clk_sys, duty cycle 15%
+    input wire          rst_n       ,
+
+    input wire          start_rqst  ,
+    input wire          fin_rqst    ,
+    input wire [7:0]    inp_data    ,
+
+    output logic        data_rqst,
+    output logic        active,
+    output logic        fin_ack,
+
+    output logic        serial_hs_output
+
+    );
+
+/***********************************
+        FSM declaration
+************************************/
+
+enum logic [2:0]
+{
+    STATE_IDLE,
+    STATE_TX_GO,
+    STATE_TX_SYNC,
+    STATE_TX_ACTIVE,
+    STATE_TX_TRAIL
+} state_current, state_next;
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n) begin
+        state_current <= STATE_IDLE;
+    end else begin
+        state_current <= state_next;
+    end
+end
+
+logic tx_hs_go_timeout;
+logic tx_hs_trail_timeout;
+
+always_comb begin
+    case (state_current)
+        STATE_IDLE:
+            state_next <= start_rqst ? STATE_TX_GO : STATE_IDLE;
+
+        STATE_TX_GO:
+            state_next <= tx_hs_go_timeout ? STATE_TX_SYNC : STATE_TX_GO;
+
+        STATE_TX_SYNC:
+            state_next <= STATE_TX_ACTIVE;
+
+        STATE_TX_ACTIVE:
+            state_next <= fin_rqst ? STATE_TX_TRAIL : STATE_TX_ACTIVE;
+
+        STATE_TX_TRAIL:
+            state_next <= tx_hs_trail_timeout ? STATE_IDLE : STATE_TX_TRAIL;
+
+        default :
+            state_next <= STATE_IDLE;
+    endcase
+end
+
+assign active       = (state_current != STATE_IDLE);
+assign fin_ack      = tx_hs_trail_timeout;
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n) begin
+        data_rqst<= 1'b0;
+    end else begin
+        data_rqst <= (state_next == STATE_TX_SYNC) || (state_next == STATE_TX_ACTIVE);
+    end
+end
+
+localparam [7:0] SYNC_SEQUENCE = 8'b00011101;
+
+logic [7:0] serdes_data;
+logic [7:0] last_bit_byte;
+
+always_comb begin
+    if(state_current == STATE_TX_SYNC)
+        serdes_data <= SYNC_SEQUENCE;
+    else if(state_current == STATE_TX_ACTIVE)
+        serdes_data <= inp_data;
+    else if(state_current == STATE_TX_TRAIL)
+        serdes_data <= last_bit_byte;
+end
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n) begin
+         last_bit_byte <= 8'd0;
+    end else if(state_current == STATE_TX_ACTIVE) begin
+         last_bit_byte <= {8{!inp_data[7]}};
+    end
+end
+
+wire serdes_enable;
+wire serdes_out;
+
+assign serdes_enable = active;
+
+altlvds altlvds_inst_0 (
+    .tx_enable ( clk_latch ),
+    .tx_in ( serdes_data ),
+    .tx_inclock ( clk_serdes ),
+    .tx_out ( serdes_out )
+    );
+
+hs_buff hs_buff_inst_d0 (
+    .datain ( serdes_out ),
+    .oe ( serdes_enable ),
+    .dataout ( serial_hs_output )
+    );
+
+// Timeout
+localparam [7:0] TX_HS_GO_TIMEOUT_VAL = 20; // 145 ns + 10*UI
+localparam [7:0] TX_HS_TRAIL_TIMEOUT_VAL = 10; // 145 ns + 10*UI
+
+logic [7:0] tx_hs_go_counter;
+logic [7:0] tx_hs_trail_counter;
+
+always_ff @(posedge clk_sys or negedge rst_n)
+    if(~rst_n)                              tx_hs_go_counter <= 0;
+    else if(state_next == STATE_TX_GO)      tx_hs_go_counter <= TX_HS_GO_TIMEOUT_VAL;
+    else if(state_current == STATE_TX_GO)   tx_hs_go_counter <= tx_hs_go_counter - 1;
+
+assign tx_hs_go_timeout = (state_current == STATE_TX_GO) && !(|tx_hs_go_counter);
+
+always_ff @(posedge clk_sys or negedge rst_n)
+    if(~rst_n)                                  tx_hs_trail_counter <= 0;
+    else if(state_next == STATE_TX_TRAIL)       tx_hs_trail_counter <= TX_HS_TRAIL_TIMEOUT_VAL;
+    else if(state_current == STATE_TX_TRAIL)    tx_hs_trail_counter <= tx_hs_trail_counter - 1;
+
+assign tx_hs_trail_timeout = (state_current == STATE_TX_TRAIL) && !(|tx_hs_trail_counter);
+
+
+endmodule // dsi_hs_lane
---
+++ rtl/dsi_lane_full.sv
@@ -0,0 +1,148 @@
+module dsi_lane_full (
+    input wire          clk_sys     , // serial data clock
+    input wire          clk_serdes  , // logic clock = clk_hs/8
+    input wire          clk_latch   , // clk_sys, duty cycle 15%
+    input wire          rst_n       ,
+
+    input wire          start_rqst  ,
+    input wire          fin_rqst    ,
+    input wire [7:0]    inp_data    ,
+
+    output logic        data_rqst,
+    output logic        active,
+
+    output logic        serial_hs_output,
+    output logic        LP_p_output,
+    output logic        LP_n_output
+);
+
+logic fin_ack;
+
+/***********************************
+        FSM declaration
+************************************/
+
+enum logic [2:0]
+{
+    STATE_IDLE,
+    STATE_HS_RQST,
+    STATE_HS_PREP,
+    STATE_HS_ACTIVE,
+    STATE_HS_EXIT,
+} state_current, state_next;
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n) begin
+        state_current <= STATE_IDLE;
+    end else begin
+        state_current <= state_next;
+    end
+end
+
+always_comb begin
+    case (state_current)
+        STATE_IDLE:
+            state_next <= start_rqst ? STATE_HS_RQST : STATE_IDLE;
+
+        STATE_HS_RQST:
+            state_next <= hs_rqst_timeout ? STATE_HS_PREP : STATE_HS_RQST;
+
+        STATE_HS_PREP:
+            state_next <= hs_prep_timeout ? STATE_HS_ACTIVE : STATE_HS_PREP;
+
+        STATE_HS_ACTIVE:
+            state_next <= hs_fin_ack ? STATE_HS_EXIT : STATE_HS_ACTIVE;
+
+        STATE_HS_EXIT:
+            state_next <= hs_exit_timeout ? STATE_IDLE : STATE_HS_ACTIVE;
+
+        default :
+            state_next <= STATE_IDLE;
+    endcase
+end
+
+logic LP_p;
+logic LP_n;
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n)                              LP_p <= 1;
+    else if(state_next == STATE_IDLE)       LP_p <= 1;
+    else if(state_next == STATE_HS_RQST)    LP_p <= 0;
+end
+
+always_ff @(posedge clk_sys or negedge rst_n) begin
+    if(~rst_n)                              LP_n <= 1;
+    else if(state_next == STATE_IDLE)       LP_n <= 1;
+    else if(state_next == STATE_HS_PREP)    LP_n <= 0;
+end
+
+logic lp_lines_enable;
+
+assign lp_lines_enable = (state_current != STATE_HS_ACTIVE);
+
+lp_buff lp_buff_inst_p (
+    .datain     ( LP_p              ),
+    .oe         ( lp_lines_enable   ),
+    .dataout    ( LP_p_output       )
+    );
+
+lp_buff lp_buff_inst_n (
+    .datain     ( LP_n              ),
+    .oe         ( lp_lines_enable   ),
+    .dataout    ( LP_n_output       )
+    );
+
+/******* Timeouts *******/
+
+localparam [7:0] T_LPX          = 100;  // 50 ns
+localparam [7:0] T_HS_PREPARE   = 50;   // 40 ns + 4*UI  :  85 ns + 6*UI
+localparam [7:0] T_HS_EXIT      = 100;  // 100 ns
+
+logic hs_rqst_timeout;
+logic hs_prep_timeout;
+logic hs_exit_timeout;
+
+logic [7:0] hs_rqst_counter;
+logic [7:0] hs_prep_counter;
+logic [7:0] hs_exit_counter;
+
+always_ff @(posedge clk_sys or negedge rst_n)
+    if(~rst_n)                              hs_rqst_counter <= 0;
+    else if(state_next == STATE_HS_RQST)    hs_rqst_counter <= T_LPX;
+    else if(state_current == STATE_HS_RQST) hs_rqst_counter <= hs_rqst_counter - 1;
+
+assign hs_rqst_timeout = (state_current == STATE_HS_RQST) && !(|hs_rqst_counter);
+
+always_ff @(posedge clk_sys or negedge rst_n)
+    if(~rst_n)                              hs_prep_counter <= 0;
+    else if(state_next == STATE_HS_PREP)    hs_prep_counter <= T_LPX;
+    else if(state_current == STATE_HS_PREP) hs_prep_counter <= hs_prep_counter - 1;
+
+assign hs_prep_timeout = (state_current == STATE_HS_PREP) && !(|hs_prep_counter);
+
+always_ff @(posedge clk_sys or negedge rst_n)
+    if(~rst_n)                              hs_exit_counter <= 0;
+    else if(state_next == STATE_HS_EXIT)    hs_exit_counter <= T_LPX;
+    else if(state_current == STATE_HS_EXIT) hs_exit_counter <= hs_exit_counter - 1;
+
+assign hs_exit_timeout = (state_current == STATE_HS_EXIT) && !(|hs_exit_counter);
+
+dsi_hs_lane dsi_hs_lane_0(
+    .clk_sys                (clk_sys            ), // serial data clock
+    .clk_serdes             (clk_serdes         ), // logic clock = clk_hs/8
+    .clk_latch              (clk_latch          ), // clk_sys, duty cycle 15%
+    .rst_n                  (rst_n              ),
+
+    .start_rqst             (start_rqst         ),
+    .fin_rqst               (fin_rqst           ),
+    .inp_data               (inp_data           ),
+
+    .data_rqst              (data_rqst          ),
+    .active                 (active             ),
+    .fin_ack                (fin_ack            ),
+
+    .serial_hs_output       (serial_hs_output   )
+
+    );
+
+endmodule
