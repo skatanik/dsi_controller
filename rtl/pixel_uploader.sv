986e5e48bd7de56117c4e09b1c709688c80580bf
skatanik <muxaal@mail.ru>
2018-10-03 23:16:56 +0300

    [rtl] pixel uploader update

--- rtl/pixel_uploader.sv
+++ rtl/pixel_uploader.sv
@@ -69,8 +69,8 @@ assign next_state_idle     = (state_current == STATE_REPACK_DATA) && (state_next
 assign next_state_read     = (state_current == STATE_IDLE) && (state_next == STATE_READ_DATA);
 assign next_state_repack   = (state_current == STATE_READ_DATA) && (state_next == STATE_REPACK_DATA);
 
-always_ff @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))   state_current <= STATE_IDLE;
+always_ff @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))     state_current <= STATE_IDLE;
     else                state_current <= state_next;
 
 /********* Read memory, repack data, write it to fifo, repeat *********/
@@ -95,8 +95,8 @@ always_comb
 /********* Data reader *********/
 logic enable_reg;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))   enable_reg <= 1'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))     enable_reg <= 1'b0;
     else                enable_reg <= enable;
 
 logic           initial_write_address;
@@ -104,8 +104,8 @@ logic [31:0]    base_address_reg;
 
 assign initial_write_address = (enable_reg ^ enable) & enable;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))                   base_address_reg <= 'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))                     base_address_reg <= 'b0;
     else if(initial_write_address)      base_address_reg <= base_address;
 
 logic [31:0]    current_addess;
@@ -113,8 +113,8 @@ logic           reset_current_address;
 
 assign reset_current_address = next_state_idle & (current_addess == (base_address_reg + total_size));
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))                   current_addess <= 'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))                     current_addess <= 'b0;
     else if(reset_current_address)      current_addess <= base_address_reg;
     else if(initial_write_address)      current_addess <= base_address;
     else if(next_state_repack)          current_addess <= current_addess + 32'd32 * (word_mode ? 32'd1 : 32'd4);
@@ -125,13 +125,13 @@ logic data_ready_delayed;
 
 assign data_ready = avl_mm_read_reg & !avl_mm_waitrequest;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))           avl_mm_read_reg <= 1'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))             avl_mm_read_reg <= 1'b0;
     else if(next_state_read)    avl_mm_read_reg <= 1'b1;
     else if(data_ready)         avl_mm_read_reg <= 1'b0;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))   data_ready_delayed <= 1'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))     data_ready_delayed <= 1'b0;
     else                data_ready_delayed <= data_ready;
 
 logic read_data;
@@ -141,15 +141,15 @@ assign read_error   = avl_mm_readdatavalid & (|avl_mm_response);
 assign read_data    = data_ready_delayed & avl_mm_readdatavalid & !(|avl_mm_response);
 assign read_done    = read_data;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))       read_error_reg <= 1'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))         read_error_reg <= 1'b0;
     else if(read_error)     read_error_reg <= 1'b1;
     else if(!enable)        read_error_reg <= 1'b0;
 
 logic [255:0] data_reg;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))       data_reg <= 'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))         data_reg <= 'b0;
     else if(read_data)      data_reg <= avl_mm_readdata;
 
 /********* Repacker 32 to 4 *********/
@@ -158,38 +158,34 @@ always @(`CLK_RST(clk, reset_n))
 logic [191:0] data_vector_remapped;
 
 genvar i, j;
-
 generate
-    for(i = 0, j = 0; i < 32; i = i + 1)
-    begin
-        if(i%4 != 3)
-        begin
-            assign data_vector_remapped[j*8 +:8]  = data_reg[i*8+: 8];
-            j = j + 1;
-        end
+    for(i = 0; i < 8; i = i + 1)
+    begin: remap
+        for(j=i*4 - i; j < i*4 - i + 3; j = j + 1)
+            assign data_vector_remapped[8*j+:8] = data_reg[(j+i)*8+:8];
     end
 endgenerate
 
 logic [2:0] words_counter;
 logic       write_fifo_reg;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))           words_counter <= 2'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))             words_counter <= 2'b0;
     else if(write_fifo_reg)     words_counter <= words_counter + 2'd1;
     else if(next_state_idle)    words_counter <= 2'b0;
 
 logic [31:0] fifo_register;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))           fifo_register <= 32'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))             fifo_register <= 32'b0;
     else if(write_fifo_reg)     fifo_register <= transform_data ? data_vector_remapped[32*words_counter+:32] : data_reg[32*words_counter+:32];
 
 assign write_fifo_reg = (state_current == STATE_REPACK_DATA) && (words_counter <= (transform_data ? 5'd5 : 5'd7)) && (!pix_fifo_full);
 
 logic fifo_buff_write;
 
-always @(`CLK_RST(clk, reset_n))
-    if(`RST(reset_n))           fifo_buff_write <= 1'b0;
+always @(`CLK_RST(clk, rst_n))
+    if(`RST(rst_n))             fifo_buff_write <= 1'b0;
     else if(!pix_fifo_full)     fifo_buff_write <= write_fifo_reg;
 
 assign pix_fifo_write           = fifo_buff_write & (!pix_fifo_full);
